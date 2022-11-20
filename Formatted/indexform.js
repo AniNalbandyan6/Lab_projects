bc234beb3290b3eac9066879f674686da9a0b822
AniNalbandyan6 <118391622+AniNalbandyan6@users.noreply.github.com>
2022-11-20 20:58:13 +0400

    Add files via upload

---
+++ Formatted/indexform.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const {
+    parseJsonFile,
+    changeJsonFile,
+    editObjectNestedValue
+} = require("./main")
+
+// to get parameters
+const changedParameters = [...process.argv.splice(2)].map(param => param.split("="));
+
+// to read the file
+const userJson = parseJsonFile("./example.json");
+
+// to change properties
+for (let [key, value] of changedParameters) {
+    if (key.includes(".")) {
+        editObjectNestedValue(userJson, key, value);
+    } else {
+        userJson[key] = value;
+    }
+}
+
+// to save changed properties
+changeJsonFile("./example.json", userJson);
